d6a09077c7f73aa1fbe2625b7b3d573902e4d3d6
Sean Boisen <sean@seanboisen.com>
2025-05-16 15:36:24 -0700

    export package constants

--- commoneval/__init__.py
+++ commoneval/__init__.py
@@ -5,3 +5,10 @@ DATAPATH = ROOT / "data"
 # where English datasets go
 DATAENGPATH = ROOT / "data/eng"
 SRCPATH = ROOT / "src"
+
+__all__ = [
+    "ROOT",
+    "DATAPATH",
+    "DATAENGPATH",
+    "SRCPATH",
+]
